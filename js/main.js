ed80f249269a1e1cbc397369a8c2338a342dcd83
Linh Vu <linh777gkog@gmail.com>
2022-07-11 23:21:54 +0700

    action create suqare

--- js/main.js
+++ js/main.js
@@ -115,3 +115,15 @@ document.addEventListener("DOMContentLoaded", () => {
         window.alert("Word is not recognised!");
       });
   }
+
+    function createSquares() {
+    const gameBoard = document.getElementById("board");
+
+    for (let index = 0; index < 30; index++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.classList.add("animate__animated");
+      square.setAttribute("id", index + 1);
+      gameBoard.appendChild(square);
+    }
+  }
